1d510bad7642218d9bf89a5fa62cb18bcfa88a7e
Franco Caraballo <franconcaraballo@gmail.com>
2024-07-08 10:35:33 -0300

    Sneakers class

---
+++ src/main/java/com/mycompany/finalprojectcac/Sneakers.java
@@ -0,0 +1,69 @@
+
+package com.mycompany.finalprojectcac;
+
+public class Sneakers {
+    private int id_sneaker;
+    private String brand;
+    private String model;
+    private String img;
+    private int price;
+    
+    // Constructores
+    public Sneakers(){
+    }
+
+    public Sneakers(int id_sneaker, String brand, String model, String img, int price) {
+        this.id_sneaker = id_sneaker;
+        this.brand = brand;
+        this.model = model;
+        this.img = img;
+        this.price = price;
+    }
+    
+    // Getters and Setters
+
+    public int getId_sneaker() {
+        return id_sneaker;
+    }
+
+    public void setId_sneaker(int id_sneaker) {
+        this.id_sneaker = id_sneaker;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    
+    
+    
+    
+}
