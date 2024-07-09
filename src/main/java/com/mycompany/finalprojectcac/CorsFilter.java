9d51d24e5c6cc2979a7de8b0de05ea20567e4095
Franco Caraballo <franconcaraballo@gmail.com>
2024-07-09 00:09:53 -0300

    CORS

--- src/main/java/com/mycompany/finalprojectcac/CorsFilter.java
+++ src/main/java/com/mycompany/finalprojectcac/CorsFilter.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.finalprojectcac;
 
-/**
- *
- * @author franc
- */
-public class CorsFilter {
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);     
+    }
+    
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) res;
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "content-Type, Authorization");
+        httpResponse.setHeader("Access-Control-Max-Age", "600");
+        
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        
+        fc.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+    
     
 }
